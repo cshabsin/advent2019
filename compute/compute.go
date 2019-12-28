9de16d16bd28219779a2dd169681be8996951864
Chris Shabsin <cshabsin@gmail.com>
2019-12-27 23:38:03 -0500

    Add I/O

--- compute/compute.go
+++ compute/compute.go
@@ -2,12 +2,15 @@ package compute
 
 import "fmt"
 
-func Run(instructions []int) ([]int, error) {
-	buffer := make([]int, 0, len(instructions))
-	for _, b := range instructions {
+func Run(inbuf, inputs []int) ([]int, []int, error) {
+	// copy the buffer to leave original unchanged.
+	buffer := make([]int, 0, len(inbuf))
+	for _, b := range inbuf {
 		buffer = append(buffer, b)
 	}
+	outbuf := make([]int, 0)
 	ip := 0
+	inptr := 0
 	for {
 		var opwidth int
 		opcode := buffer[ip] % 100
@@ -17,11 +20,11 @@ func Run(instructions []int) ([]int, error) {
 			// ADD
 			a, err := modes.evalParam(0, buffer, buffer[ip+1])
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 			b, err := modes.evalParam(1, buffer, buffer[ip+2])
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 			buffer[buffer[ip+3]] = a + b
 			opwidth = 4
@@ -29,19 +32,36 @@ func Run(instructions []int) ([]int, error) {
 			// MUL
 			a, err := modes.evalParam(0, buffer, buffer[ip+1])
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 			b, err := modes.evalParam(1, buffer, buffer[ip+2])
 			if err != nil {
-				return nil, err
+				return nil, nil, err
 			}
 			buffer[buffer[ip+3]] = a * b
 			opwidth = 4
+		case 3:
+			// SAV
+			if inptr >= len(inputs) {
+				return nil, nil, fmt.Errorf("SAV: Read past end of input at IP %d", ip)
+			}
+			val := inputs[inptr]
+			inptr++
+			buffer[buffer[ip+1]] = val
+			opwidth = 2
+		case 4:
+			// OUT
+			a, err := modes.evalParam(0, buffer, buffer[ip+1])
+			if err != nil {
+				return nil, nil, err
+			}
+			outbuf = append(outbuf, a)
+			opwidth = 2
 		case 99:
 			// EXIT
-			return buffer, nil
+			return buffer, outbuf, nil
 		default:
-			return nil, fmt.Errorf("Invalid opcode %d at IP %d", buffer[ip], ip)
+			return nil, nil, fmt.Errorf("Invalid opcode %d at IP %d", buffer[ip], ip)
 		}
 		ip += opwidth
 	}
--- compute/compute_test.go
+++ compute/compute_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestCompute(t *testing.T) {
+func TestComputeNoIO(t *testing.T) {
 	testcases := []struct {
 		desc string
 		in   []int
@@ -16,15 +16,58 @@ func TestCompute(t *testing.T) {
 			in:   []int{1, 0, 0, 0, 99},
 			want: []int{2, 0, 0, 0, 99},
 		},
+		{
+			desc: "immediate",
+			in:   []int{101, 4, 0, 0, 99},
+			want: []int{105, 4, 0, 0, 99},
+		},
 	}
 	for _, tc := range testcases {
-		got, err := Run(tc.in)
+		got, _, err := Run(tc.in, nil)
 		if err != nil {
 			t.Errorf("Run(%q): unexpected error: %v", tc.desc, err)
 			continue
 		}
 		if diff := cmp.Diff(tc.want, got); diff != "" {
 			t.Errorf("Run(%q): incorrect result, -want +got:\n%s", tc.desc, diff)
+		}
+	}
+}
+
+func TestCompute(t *testing.T) {
+	testcases := []struct {
+		desc    string
+		inbuf   []int
+		inputs  []int
+		want    []int
+		wantout []int
+	}{
+		{
+			desc:    "input",
+			inbuf:   []int{3, 3, 99, 0},
+			inputs:  []int{1},
+			want:    []int{3, 3, 99, 1},
+			wantout: []int{},
+		},
+		{
+			desc:    "output",
+			inbuf:   []int{104, 37, 99},
+			inputs:  []int{},
+			want:    []int{104, 37, 99},
+			wantout: []int{37},
+		},
+	}
+	for _, tc := range testcases {
+		got, gotout, err := Run(tc.inbuf, tc.inputs)
+		if err != nil {
+			t.Errorf("Run(%q): unexpected error: %v", tc.desc, err)
+			continue
+		}
+		if diff := cmp.Diff(tc.want, got); diff != "" {
+			t.Errorf("Run(%q): incorrect result, -want +got:\n%s", tc.desc, diff)
+		}
+		if diff := cmp.Diff(tc.wantout, gotout); diff != "" {
+			t.Errorf("Run(%q): incorrect output, -want +got:\n%s", tc.desc, diff)
 		}
 	}
 }
