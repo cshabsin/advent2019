022f94052ee19b50f79f370119e89aefaf6aeda0
Chris Shabsin <cshabsin@gmail.com>
2020-01-02 04:23:24 -0500

    process grid a little

--- day17/day17.go
+++ day17/day17.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 
@@ -17,16 +19,40 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	board := [][]int{nil}
 	aIO, bIO := compute.NewChanIO()
 	go func() {
 		for {
 			val, err := aIO.Read()
+			if errors.Is(err, io.EOF) {
+				return
+			}
 			if err != nil {
 				fmt.Printf("aIO.Read: %v", err)
 				return
 			}
-			fmt.Printf("%c", rune(val))
+			lastRow := len(board) - 1
+			if val == 10 {
+				board = append(board, nil)
+				fmt.Printf("\n")
+			} else {
+				var bVal int
+				if val == 35 { // pound
+					bVal = 1
+				} else if val == 46 { // period
+					bVal = 0
+				} else if val == 94 { // caret
+					bVal = 2
+				} else if val == 60 { // less than
+					bVal = 2
+				} else if val == 62 { // greater than
+					bVal = 2
+				} else if val == 118 { // v
+					bVal = 2
+				}
+				board[lastRow] = append(board[lastRow], bVal)
+				fmt.Printf("%c", []rune{'.', '#', '*', '?'}[bVal])
+			}
 		}
 	}()
 	intcode := compute.NewIntcode(buf, bIO)
@@ -34,5 +60,5 @@ func main() {
 		fmt.Printf("compute.Run: %v\n", err)
 		return
 	}
-	
+
 }
